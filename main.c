f2336df09ff6fee65e4bb556955661dbe8ecf940
Doykim <boradal39@gmail.com>
2020-02-26 01:45:33 +0900

    to test bzero

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-void *ft_memset(void *s, int c, unsigned int n);
+void	*ft_memset(void *s, int c, unsigned int n);
+void	ft_bzero(void *b, unsigned int n);
 unsigned int	ft_strlen(const char *s);
 unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size);
 
 int	main()
 {
-	const char *s;
-	char	*dst[5];
+	char	dst[6];
+	int i = 0;
 
-	ft_memset(dst, 0x41, 5);
-	s = "Hello 42!";
-	printf("%s\n", dst);
+	while (i < 5)
+	{
+		printf("%d %d %d\n", i, dst + i, *(dst + i));
+		i++;
+	}
+	i = 0;
+	ft_bzero(dst, 5);
+	printf("a%sa\n", dst);
+	while (i < 5)
+	{
+		printf("%d %d %d\n", i , dst + i, *(dst + i));
+		i++;
+	}
 }
