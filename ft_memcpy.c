5dd6ace1d14172af2fee694813b07fbf1aa2af32
Doykim <boradal39@gmail.com>
2020-02-26 02:15:20 +0900

    Mod memcpy : Seg fault occurs

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+
 void	*ft_memcpy(void *dst, const void *src, unsigned int n)
 {
 	unsigned int	i;
 	char	*c_src;
+	char	*c_dst;
 	
 	i = 0;
 	c_src = (char *)src;
-	while (i < n && *(c_src + i) != '\0')
+	c_dst = (char *)dst;
+	while (i < n && c_src[i] != '\0')
 	{
-		*((char *)dst) = *(c_src + i);
+		printf("%d %c\n", i, c_src[i]);
+		c_dst[i] = c_src[i];
+		i++;
 	}
 	return (dst);
 }
