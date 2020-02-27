8950b89bbbc0f1966282df2e4b457ffad3aeeaf7
Doykim <boradal39@gmail.com>
2020-02-27 18:42:38 +0900

    Fix strlcpy - Seg fault : it was main.c problem

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
-
 unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (i < size && *(src + i) != '\0')
+	while (i < size - 1 && *(src + i) != '\0')
 	{
-		printf("%d...%c...%c\n", i, dst[i], src[i]);
 		dst[i] = src[i];
 		i++;
 	}
 	*(dst + i) = '\0';
 	while (*(src + i) != '\0')
 	{
-		printf("%d...\n", i);
 		i++;
 	}
 	return (i);
